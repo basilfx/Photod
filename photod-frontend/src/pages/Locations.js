75bba6f8f2192af42b777a0fe133bc467c1e4d7e
Bas Stottelaar <basstottelaar@gmail.com>
2017-07-14 00:56:48 +0200

    Fix URLs.

--- photod-frontend/src/pages/Locations.js
+++ photod-frontend/src/pages/Locations.js
@@ -70,8 +70,8 @@ export default class Locations extends React.Component<DefaultProps, Props, void
             >
                 <Map center={[51.98510, 5.89872]} zoom={9} className='uk-height-1-1'>
                     <TileLayer
-                        attribution='&amp;copy <a href=&quot;//osm.org/copyright&quot;>OpenStreetMap</a> contributors'
-                        url='//{s}.tile.osm.org/{z}/{x}/{y}.png'
+                        attribution={`© <a href="${document.location.protocol}//osm.org/copyright">OpenStreetMap</a> contributors`}
+                        url={`${document.location.protocol}//{s}.tile.osm.org/{z}/{x}/{y}.png`}
                     />
                 </Map>
             </Main>
