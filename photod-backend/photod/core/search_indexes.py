cdcb2e4395c9efa2bd8d2a52cdf3416c9f3f59fc
Bas Stottelaar <basstottelaar@gmail.com>
2017-07-13 18:15:25 +0200

    Add templates for indexing models.

--- photod-backend/photod/core/search_indexes.py
+++ photod-backend/photod/core/search_indexes.py
@@ -2,43 +2,29 @@ from haystack import indexes
 
 from photod.core.models import MediaFile, Tag, Person
 
-import datetime
-
 
 class MediaFileIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-
-    path = indexes.CharField(model_attr='path')
-    mime_type = indexes.CharField(model_attr='mime_type')
+    text = indexes.CharField(
+        document=True, use_template=True,
+        template_name="search/media_file.txt")
 
     recorded = indexes.DateTimeField(model_attr='recorded')
 
     def get_model(self):
         return MediaFile
 
-    def index_queryset(self, using=None):
-        return self.get_model().objects.filter(
-            created__lte=datetime.datetime.now()
-        )
-
 
 class TagIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    label = indexes.CharField(model_attr='label')
+    text = indexes.CharField(
+        document=True, use_template=True, template_name="search/tag.txt")
 
     def get_model(self):
         return Tag
 
-    def index_queryset(self, using=None):
-        return self.get_model().objects.all()
-
 
 class PersonIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    name = indexes.CharField(model_attr='name')
+    text = indexes.CharField(
+        document=True, use_template=True, template_name="search/person.txt")
 
     def get_model(self):
         return Person
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.all()
