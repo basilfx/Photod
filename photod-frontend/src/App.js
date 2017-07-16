0ea11d2cebc0980c8a7031556e249b44d5745cc6
Bas Stottelaar <basstottelaar@gmail.com>
2017-07-16 20:51:57 +0200

    Restructure paths so that you do not have to click twice in the tree view.

--- photod-frontend/src/App.js
+++ photod-frontend/src/App.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Albums from 'pages/Albums';
 import Directories from 'pages/Directories';
@@ -20,35 +20,29 @@ export default class App extends React.Component {
         return (
             <Router>
                 <Switch>
-                    <Route exact path='/' component={Directories} />
+                    <Redirect exact from='/' to='/directories'/>
 
-                    <Route exact path='/login' component={({ history }) => <Login history={history} />} />
-
-                    <Route exact path='/directories' component={Directories } />
-                    <Route exact path='/directories/:id' component={
-                        ({ match }) => <Directories id={toGlobalId('Directory', match.params.id)} />
+                    <Route exact path='/login' component={
+                        ({ history }) => <Login history={history} />
                     } />
 
-                    <Route exact path='/albums' component={Albums} />
-                    <Route exact path='/albums/:id' component={
-                        ({ match }) => <Albums id={toGlobalId('Album', match.params.id)} />
+                    <Route exact name='directories' path='/directories/:id?' component={
+                        ({ match }) => <Directories key='directories' id={toGlobalId('Directory', match.params.id)} />
                     } />
-
-                    <Route exact path='/tags' component={Tags} />
-                    <Route exact path='/tags/:tag' component={
-                        ({ match }) => <Tags tag={match.params.tag} />
+                    <Route exact path='/albums/:id?' component={
+                        ({ match }) => <Albums key='albums' id={toGlobalId('Album', match.params.id)} />
                     } />
-
-                    <Route exact path='/people' component={People} />
+                    <Route exact path='/tags/:tag?' component={
+                        ({ match }) => <Tags key='tags' tag={match.params.tag} />
+                    } />
                     <Route exact path='/people/:id' component={
-                        ({ match }) => <People id={toGlobalId('Person', match.params.id)} />
+                        ({ match }) => <People key='people' id={toGlobalId('Person', match.params.id)} />
                     } />
-
-                    <Route exact path='/locations' component={Locations} />
-
-                    <Route exact path='/settings' component={Settings} />
-                    <Route exact path='/settings/:page' component={
-                        ({ match }) => <Settings page={match.params.page} />
+                    <Route exact path='/locations' component={
+                        ({ match }) => <Locations key='locations' />
+                    } />
+                    <Route exact path='/settings/:page?' component={
+                        ({ match }) => <Settings key='settings' page={match.params.page} />
                     } />
 
                     <Route component={NotFound} />
