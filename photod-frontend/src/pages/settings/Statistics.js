69b7b7a09674d587ef99776c9a8b66aa031a7e4b
Bas Stottelaar <basstottelaar@gmail.com>
2017-07-23 15:56:26 +0200

    Fix className warning.

--- photod-frontend/src/pages/settings/Statistics.js
+++ photod-frontend/src/pages/settings/Statistics.js
@@ -44,12 +44,12 @@ export default class Statistics extends React.Component<DefaultProps, Props, voi
             <div className='uk-padding-small'>
                 <p className='uk-text-lead'>Current profile</p>
 
-                <dl class='uk-description-list'>
+                <dl className='uk-description-list'>
                     <dt>Minimal quality</dt>
                     <dd>{profile.quality}</dd>
                 </dl>
 
-                <dl class='uk-description-list'>
+                <dl className='uk-description-list'>
                     <dt>Preferred MIME-types</dt>
                     <dd>{profile.mimeTypes.join(', ')}</dd>
                 </dl>
