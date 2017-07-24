fc779ef8f81fb390993c06529aade0f95aad6861
Bas Stottelaar <basstottelaar@gmail.com>
2017-07-24 10:32:57 +0200

    Add DateTime component

---
+++ photod-frontend/src/components/DateTime.js
@@ -0,0 +1,74 @@
+// @flow
+
+import React from 'react';
+
+import moment from 'moment-timezone';
+
+/**
+ * Type declaration for Props.
+ */
+type Props = {
+    format?: string,
+    relative?: boolean,
+    timestamp: string,
+};
+
+/**
+ * Type declaration for DefaultProps.
+ */
+type DefaultProps = {
+    format: string,
+    relative: boolean,
+};
+
+/**
+ * Format a given timestamp in a human-readable format.
+ *
+ * Refer to the documentation of Moment.js (http://momentjs.com/) for the
+ * different formatting options.
+ *
+ * This component takes the timezone from `window.getApplicationTimezone()`.
+ */
+export default class DateTime extends React.PureComponent<DefaultProps, Props, void> {
+    /**
+     * @inheritdoc
+     */
+    props: Props;
+
+    /**
+     * @inheritdoc
+     */
+    static defaultProps = {
+        format: 'LLL',
+        relative: true,
+    };
+
+    /**
+     * @inheritdoc
+     */
+    render() {
+        const timestamp = this.props.timestamp;
+
+        if (!timestamp) {
+            return <span>—</span>;
+        }
+
+        let dateTime;
+
+        if (typeof timestamp === 'string') {
+            dateTime = moment.tz(timestamp, window.getApplicationTimezone());
+        }
+        else {
+            throw new Error('Timestamp must be an object or a string.');
+        }
+
+        const title = dateTime.format(this.props.format);
+        const content = this.props.relative ? dateTime.fromNow() : title;
+
+        return (
+            <span title={title}>
+                {content}
+            </span>
+        );
+    }
+}
