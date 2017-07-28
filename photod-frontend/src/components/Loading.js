e99edf7354a47aab5f91bbbb5bee71d9122fa0e4
Bas Stottelaar <basstottelaar@gmail.com>
2017-07-28 14:02:39 +0200

    Add loading component.

---
+++ photod-frontend/src/components/Loading.js
@@ -0,0 +1,21 @@
+// @flow
+
+import React from 'react';
+
+import Spinner from 'ui/Spinner';
+
+/**
+ * The component.
+ */
+export default class Loading extends React.Component<void, void, void> {
+    /**
+     * @inheritdoc
+     */
+    render() {
+        return (
+            <div className='uk-padding-small uk-height-1-1 uk-flex uk-flex-center uk-flex-middle uk-flex-column'>
+                <Spinner size={2}/>
+            </div>
+        );
+    }
+}
