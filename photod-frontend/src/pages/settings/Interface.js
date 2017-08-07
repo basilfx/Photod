dcbffd1aaee336711cb3e54c242897effdabaf40
Bas Stottelaar <basstottelaar@gmail.com>
2017-08-07 19:21:53 +0200

    Implement interface settings page.

--- photod-frontend/src/pages/settings/Interface.js
+++ photod-frontend/src/pages/settings/Interface.js
@@ -2,6 +2,8 @@
 
 import autobind from 'autobind-decorator';
 
+import { connect } from 'react-redux';
+
 import React from 'react';
 
 import Button from 'ui/Button';
@@ -10,24 +12,39 @@ import Form from 'components/form/Form';
 import FormGroup from 'components/form/FormGroup';
 import Input from 'components/form/Input';
 
+import { set } from 'modules/settings/actions';
+
+import UIkit from 'uikit';
+
+import type { Values } from 'components/form/types';
+
 /**
  * Type declaration for Props.
  */
 type Props = {
-    // children?: React.Element<*>,
+    settings: {
+        [string]: mixed,
+    },
 };
 
 /**
  * Type declaration for DefaultProps.
  */
 type DefaultProps = {
-    // TODO
+
+};
+
+/**
+ * Type declaration for State.
+ */
+type State = {
+    [string]: mixed,
 };
 
 /**
  * The component.
  */
-export default class Interface extends React.Component<DefaultProps, Props, void> {
+class Interface extends React.Component<DefaultProps, Props, State> {
     /**
      * @inheritdoc
      */
@@ -36,12 +53,53 @@ export default class Interface extends React.Component<DefaultProps, Props, void
     /**
      * @inheritdoc
      */
+    state: State;
+
+    /**
+     * @inheritdoc
+     */
     static defaultProps = {
 
     };
 
-    @autobind async handleValidSubmit(values: Object) {
+    /**
+     * @inheritdoc
+     */
+    constructor(props: Props) {
+        super(props);
 
+        this.state = {
+            'application.menu.autoExpand':
+                props.settings['application.menu.autoExpand'],
+            'application.mediaList.hoverShadow':
+                props.settings['application.mediaList.hoverShadow'],
+            'application.lightbox.blur':
+                props.settings['application.lightbox.blur'],
+        };
+    }
+
+    /**
+     * Handle form field change.
+     */
+    @autobind handleChange(name: string, value: mixed) {
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    /**
+     * Handle form submission.
+     *
+     * This will persist the settings.
+     */
+    @autobind handleValidSubmit(values: Values): mixed {
+        this.props.set(values);
+
+        return UIkit.notification({
+            message: 'Settings saved.',
+            pos: 'bottom-left',
+            status: 'success',
+        });
     }
 
     /**
@@ -54,19 +112,19 @@ export default class Interface extends React.Component<DefaultProps, Props, void
                     <h4>Menu</h4>
 
                     <label>
-                        <Input type='checkbox' name='autoExpand' /> Auto-expand treeview items with only one child.
+                        <Input type='checkbox' name='application.menu.autoExpand' onChange={this.handleChange} value={this.state['application.menu.autoExpand']} /> Auto-expand treeview items with only one child.
                     </label>
 
                     <h4>Media list</h4>
 
                     <label>
-                        <Input type='checkbox' name='hoverShadow' /> Disable shadow effect on hover.
+                        <Input type='checkbox' name='application.mediaList.hoverShadow' onChange={this.handleChange} value={this.state['application.mediaList.hoverShadow']} /> Disable shadow effect on hover.
                     </label>
 
                     <h4>Lightbox</h4>
 
                     <label>
-                        <Input type='checkbox' name='lightBox' /> Use black background.
+                        <Input type='checkbox' name='application.lightbox.blur' onChange={this.handleChange} value={this.state['application.lightbox.blur']} /> Add blur background.
                     </label>
 
                     <p>
@@ -79,3 +137,14 @@ export default class Interface extends React.Component<DefaultProps, Props, void
         );
     }
 }
+
+export default connect(
+    (state, props: Props) => ({
+        settings: state.settings,
+    }),
+    (dispatch, props: Props) => ({
+        set(settings) {
+            return dispatch(set(settings));
+        },
+    })
+)(Interface);
