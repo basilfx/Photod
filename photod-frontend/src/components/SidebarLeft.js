66be363388e45168a9cb4faea0056fff8f26b0e6
Bas Stottelaar <basstottelaar@gmail.com>
2017-08-07 20:23:56 +0200

    Collapsible side panels.

--- photod-frontend/src/components/SidebarLeft.js
+++ photod-frontend/src/components/SidebarLeft.js
@@ -1,8 +1,10 @@
 // @flow
 
-// import autobind from 'autobind-decorator';
+import autobind from 'autobind-decorator';
 
 import React from 'react';
+
+import Icon from 'ui/Icon';
 
 /**
  * Type declaration for Props.
@@ -20,13 +22,25 @@ type DefaultProps = {
 };
 
 /**
+ * Type declaration for State.
+ */
+type State = {
+    expanded: boolean,
+};
+
+/**
  * The component.
  */
-export default class SidebarLeft extends React.Component<DefaultProps, Props, void> {
+export default class SidebarLeft extends React.Component<DefaultProps, Props, State> {
     /**
      * @inheritdoc
      */
     props: Props;
+
+    /**
+     * @inheritdoc
+     */
+    state: State;
 
     /**
      * @inheritdoc
@@ -38,14 +52,34 @@ export default class SidebarLeft extends React.Component<DefaultProps, Props, vo
     /**
      * @inheritdoc
      */
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            expanded: true,
+        };
+    }
+
+    @autobind handleToggle() {
+        this.setState({
+            expanded: !this.state.expanded,
+        });
+    }
+
+    /**
+     * @inheritdoc
+     */
     render() {
         return (
             <aside className='tm-sidebar-left'>
                 <div className='tm-sidebar-left-menu' data-panel={!!this.props.panel}>
                     {this.props.menu}
                 </div>
-                {this.props.panel && <div className='tm-sidebar-left-panel uk-overflow-auto'>
-                    {this.props.panel}
+                {this.props.panel && <div className={`tm-sidebar-left-panel tm-sidebar-left-panel-${this.state.expanded ? 'expanded' : 'collapsed'} uk-overflow-auto`}>
+                    <div onClick={this.handleToggle} className='tm-sidebar-toggle'>
+                        <Icon icon={`chevron-${this.state.expanded ? 'left' : 'right'}`} size={1.5} />
+                    </div>
+                    {this.state.expanded && this.props.panel}
                 </div>}
             </aside>
         );
--- photod-frontend/src/components/SidebarRight.js
+++ photod-frontend/src/components/SidebarRight.js
@@ -1,8 +1,10 @@
 // @flow
 
-// import autobind from 'autobind-decorator';
+import autobind from 'autobind-decorator';
 
 import React from 'react';
+
+import Icon from 'ui/Icon';
 
 /**
  * Type declaration for Props.
@@ -19,13 +21,25 @@ type DefaultProps = {
 };
 
 /**
+ * Type declaration for State.
+ */
+type State = {
+    expanded: boolean,
+};
+
+/**
  * The component.
  */
-export default class SidebarRight extends React.Component<DefaultProps, Props, void> {
+export default class SidebarRight extends React.Component<DefaultProps, Props, State> {
     /**
      * @inheritdoc
      */
     props: Props;
+
+    /**
+     * @inheritdoc
+     */
+    state: State;
 
     /**
      * @inheritdoc
@@ -37,11 +51,31 @@ export default class SidebarRight extends React.Component<DefaultProps, Props, v
     /**
      * @inheritdoc
      */
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            expanded: true,
+        };
+    }
+
+    @autobind handleToggle() {
+        this.setState({
+            expanded: !this.state.expanded,
+        });
+    }
+
+    /**
+     * @inheritdoc
+     */
     render() {
         return (
-            <aside className="tm-sidebar-right">
-                <div className='tm-sidebar-right-panel uk-overflow-auto'>
-                    {this.props.panel}
+            <aside className='tm-sidebar-right'>
+                <div className={`tm-sidebar-right-panel tm-sidebar-right-panel-${this.state.expanded ? 'expanded' : 'collapsed'} uk-overflow-auto`}>
+                    <div onClick={this.handleToggle} className='tm-sidebar-toggle'>
+                        <Icon icon={`chevron-${this.state.expanded ? 'right' : 'left'}`} size={1.5} />
+                    </div>
+                    {this.state.expanded && this.props.panel}
                 </div>
             </aside>
         );
